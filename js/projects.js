afb0f881a861d354d962fb9bb52e63ef99887242
annagranberg <annagranberg03@gmail.com>
2026-03-15 21:57:26 +0100

    updates

---
+++ js/projects.js
@@ -0,0 +1,20 @@
+const track = document.querySelector(".slider-track");
+const slides = document.querySelectorAll(".slider img");
+const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
+
+let index = 0;
+
+function updateSlider() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener("click", () => {
+  index = (index + 1) % slides.length;
+  updateSlider();
+});
+
+prevBtn.addEventListener("click", () => {
+  index = (index - 1 + slides.length) % slides.length;
+  updateSlider();
+});
